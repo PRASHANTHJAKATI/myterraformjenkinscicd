15420e8dbd628dfc78db77a4203ec6f5dc3e015b
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-09 14:30:41 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,27 @@
-
-resource "aws_s3_bucket" "my-terraform-bucket22" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
+variable "bucket_prefix" {
+  description = "Prefix for the S3 bucket name (Terraform will add random suffix)."
+  type        = string
+  default     = "my-terraform-bucket22"
 }
+
+variable "acl" {
+  description = "Canned ACL to apply"
+  type        = string
+  default     = "private"
+}
+
+variable "versioning" {
+  description = "Enable or disable versioning"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Tags to apply to the bucket"
+  type        = map(string)
+  default = {
+    Name        = "terraform-state-bucket"
+    Environment = "dev"
+  }
+}
+
