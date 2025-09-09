67d7ed2579b4365ef9f01d75484908944cffca5b
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-09 13:23:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-bucket22"
     key            = "state/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     use_lockfile   = true   # replaces dynamodb_table
   }
 }
