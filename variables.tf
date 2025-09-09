82824319036ac131668cf9d2b676e14815ebba13
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-09 10:17:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "SSH key pair name for EC2"
+  default     = "kmaster"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for Jenkins"
+  default     = "t3.micro"
+}
+
+variable "tag_name" {
+  description = "Tag for EC2 instance"
+  default     = "JenkinsServer"
+}
+
+variable "ami_id" {
+  description = "AMI ID for Jenkins EC2 (Ubuntu recommended)"
+  default     = "ami-0c4fc5dcabc9df21d"
+}
+
+variable "security_group" {
+  description = "Security group name for Jenkins server"
+  default     = "jenkins-sg"
+}
